e6af80030d5989c09c7e19cfd4650596be8ff25c
Daniel Vianna <dvianna@ausnetservices.com.au>
2018-08-21 13:20:20 +1000

    trivial environment with pandas and postgresql

---
+++ shell.nix
@@ -0,0 +1,34 @@
+with import <nixpkgs> {};
+
+let pyenv = python36.withPackages (ps: with ps; [
+    jupyterlab
+    pandas
+    numpy
+    matplotlib
+    ]);
+    defaultDb = "data";
+   
+in
+
+stdenv.mkDerivation rec {
+  name = "python-env";
+
+  buildInputs = [
+    pyenv
+    postgresql
+    screen
+  ];
+
+  postInstall = ''
+  export PG_DATA ="data"
+  initdb -D data
+  pg_ctl -D data -l data/logfile start
+  createdb ${defaultDb}
+  '';
+  shellHook = ''
+  echo ""
+  echo "using ${pyenv.name}; ${postgresql.name}"
+  echo "default database: ${defaultDb}"
+  '';
+
+}
